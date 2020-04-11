1b9b585021dc5d2a6c0b69b405893a07fe29a805
jennymoskowitz <jmosk@umich.edu>
2020-04-10 20:44:32 -0400

    Start

--- first.py
+++ first.py
@@ -1,1 +1,19 @@
-print("hi")
+import json
+import unittest
+import os
+import requests
+
+cuisine_list = ["African", "American", "British", "Cajun", "Caribbean", "Chinese", "Eastern European", "European",
+"French", "German", "Greek", "Indian", "Irish", "Italian", "Japanese", "Jewish", "Korean", "Latin American", 
+"Mediterranean", "Mexican", "Middle Eastern", "Nordic", "Southern", "Spanish", "Thai", "Vietnamese"]
+
+food = input("What food are you looking for?")
+c = input("What cuisine are you looking for?")
+c = c[0].upper()
+if c in cuisine_list:
+    cuisine = c
+else:
+    print("Sorry, try again")
+num = 3
+base_url = https://api.spoonacular.com/recipes/search
+params = "?query" + food + "?cuisine" + cuisine + "?number" + num
